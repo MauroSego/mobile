c2e13e3822ef080c738ae4e92614ba1150108edd
MauroSego <mauro.segovia.7@gmail.com>
2015-12-28 16:12:15 -0300

    funciones con php

--- niveles_carga.php
+++ niveles_carga.php
@@ -1,4 +1,34 @@
 <?php
 include("conexion.php");
 
+  /* Empieza la sesión */
+   // session_start();
+ 
+    /* se crea la sesión */
+    //$_SESSION['username'] = $_POST['username'];
+ 
+    /* Si no hay una sesión creada, redireccionar al index. */
+    //if(empty($_SESSION['username'])) { 
+    //    header('Location: index.html');
+   // }
+
+/*$query = "INSERT INTO niveles (tx, rx,mer,usuario, fecha) 
+VALUES ('$_POST[tx]', '$_POST[rx]', '$_POST[mer]', '$_POST[username]', '$_SESSION[username]', NOW())";
+if(!$mysqli->query($query)){
+	 echo "Falló la operación: (" . $mysqli->errno . ") " . $mysqli->error;
+	}*/
+
+$query = "INSERT INTO niveles (tx) 
+VALUES (100)";
+if(!$mysqli->query($query)){
+	 echo "Falló la operación: (" . $mysqli->errno . ") " . $mysqli->error;
+	}else {
+		 echo "<script languaje='javascript'>
+    
+    location.href = 'index.html';
+   </script>";
+	}
+
+
+
 ?>
