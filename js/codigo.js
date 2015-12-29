c671b6c07f51ff16d3c1ad83c4e361290c331150
MauroSego <mauro.segovia.7@gmail.com>
2015-12-29 13:37:32 -0300

    actualizacion tamaño

---
+++ js/codigo.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	var altura = $(window).height();
+	var ancho = $(window).width();
+
+	$("#resultado").html("la altura es: " + altura+ " la anchura es: "+ ancho);
+
+
+
+})
